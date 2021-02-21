b2fd4f01a340fab92715bb6553f037a19cb10375
daichi_yamaoka <tnychor06@gmail.com>
2021-02-21 16:55:18 +0900

    第三章の演習

---
+++ capture-3/linearsearch.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func nextInt() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func main() {
+	var N int
+	N = nextInt()
+	v := nextInt()
+	a := make([]int, N)
+	for i := 0; i < N; i++ {
+		a[i] += nextInt()
+	}
+	// 線形探索
+	foundId := -1
+	for i := 0; i < N; i++ {
+		if a[i] == v {
+			foundId = i
+			break
+		}
+	}
+	fmt.Println(foundId)
+}
